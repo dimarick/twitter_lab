c9bf755f6ca6f939d171e6e281fed18747fd6b84
dimarick <dimon.ksk@gmail.com>
2025-04-09 18:43:12 +0500

    Initial commit

---
+++ schema/twitter.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS producers;
+CREATE TABLE producers
+(
+    producer_id uuid NOT NULL,
+    PRIMARY KEY (producer_id)
+);
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages
+(
+    id          uuid NOT NULL,
+    producer_id uuid NOT NULL,
+    content     varchar(140) DEFAULT NULL,
+    created     int          DEFAULT NULL,
+    PRIMARY KEY (id, producer_id),
+    CONSTRAINT fk_messages_producers1 FOREIGN KEY (producer_id) REFERENCES producers (producer_id)
+);
+
+create index fk_messages_producers_idx on messages (producer_id);
+
+DROP TABLE IF EXISTS subscribers;
+CREATE TABLE subscribers
+(
+    subscriber_id uuid NOT NULL,
+    PRIMARY KEY (subscriber_id)
+);
+
+DROP TABLE IF EXISTS subscriptions;
+CREATE TABLE subscriptions
+(
+    subscriber_id uuid NOT NULL,
+    producer_id   uuid NOT NULL,
+    PRIMARY KEY (subscriber_id, producer_id),
+    CONSTRAINT fk_subscriptions_producers FOREIGN KEY (producer_id) REFERENCES producers (producer_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_subscriptions_subscribers FOREIGN KEY (subscriber_id) REFERENCES subscribers (subscriber_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create index fk_subscriptions_producers_idx on subscriptions (producer_id);
+create index fk_subscriptions_subscribers_idx on subscriptions (subscriber_id);
+
+INSERT INTO producers (producer_id)
+VALUES ('1CD89E11602A4186AFBFE0149B59EB08'),
+       ('6E27EA06A7164C89AF88813749A8BD48');
+
+INSERT INTO subscribers (subscriber_id)
+VALUES ('70A64B5443C34C18BBEC64590FF7E0CC');
+
+INSERT INTO subscriptions (subscriber_id, producer_id)
+VALUES ('70A64B5443C34C18BBEC64590FF7E0CC', '1CD89E11602A4186AFBFE0149B59EB08');
+
+INSERT INTO messages (id, producer_id, content, created)
+VALUES ('462894B2B5A14088B7AF701C71D6D304', '6E27EA06A7164C89AF88813749A8BD48', 'Who is subscribed to Mr. Trump?',
+        '1605194769'),
+       ('B7A1C8D5C04E4FE38F986392BB751AD9', '6E27EA06A7164C89AF88813749A8BD48', 'And here Mr. Trump come again',
+        '1605194747'),
+       ('DF7E3D8FE19A4458BAD64BE7585200F5', '1CD89E11602A4186AFBFE0149B59EB08', 'Mr. Macron would like to say hello!',
+        '1605197637'),
+       ('E5C6F13A76D648B681F020B74FA9F04C', '6E27EA06A7164C89AF88813749A8BD48', 'Donald Trump posted his first message',
+        '1605194709'),
+       ('FCCC455BFF284DFD8153F07C0F869118', '1CD89E11602A4186AFBFE0149B59EB08', 'Now France President is here as well',
+        '1605195323');
---
+++ schema/ums.sql
@@ -0,0 +1,80 @@
+DROP TABLE IF EXISTS last_visit;
+
+CREATE TABLE last_visit
+(
+    id   uuid NOT NULL,
+    "in" int DEFAULT NULL,
+    out  int DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles
+(
+    id          uuid NOT NULL,
+    name        varchar(45) DEFAULT NULL,
+    description varchar(45) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users
+(
+    id            uuid        NOT NULL,
+    name          varchar(45) NOT NULL,
+    email         varchar(45) NOT NULL,
+    password      varchar(45) NOT NULL,
+    created       int         NOT NULL,
+    last_visit_id uuid DEFAULT NULL,
+    PRIMARY KEY (id),
+    foreign key (last_visit_id) references last_visit (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create unique index email_key on users (email);
+create index users_last_visit_id on users (last_visit_id);
+
+DROP TABLE IF EXISTS users_has_roles;
+CREATE TABLE users_has_roles
+(
+    users_id uuid NOT NULL,
+    roles_id uuid NOT NULL,
+    PRIMARY KEY (users_id, roles_id),
+    FOREIGN KEY (roles_id) REFERENCES roles (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+drop index fk_users_has_roles_roles1_idx;
+create index fk_users_has_roles_roles1_idx on users_has_roles (roles_id);
+
+INSERT INTO last_visit (id, "in", out)
+VALUES ('306DCF05D3D64B438E066B6FFE2331FC'::uuid, '1604249194', '1604249224'),
+       ('3C37571B0F494FED875845BFAE428B29'::uuid, '1604249181', '1604249209'),
+       ('7AC613D5538745E58C9F9F49A0F271E1'::uuid, '1604249171', '1604249203'),
+       ('8A0BE660EBC64C47AF813F7123977993'::uuid, '1604249188', '1604249217'),
+       ('BE76648F49F24FBE9BC434BD8C23C70E'::uuid, '1604130098', '1604130106');
+
+INSERT INTO roles (id, name, description)
+VALUES ('43DCF1D63AC0429099FD44F9B9F9BECF', 'ADMIN', 'Admistrative toles for the system'),
+       ('B479B3577E2547FA8DBABFDAEECC6C2C', 'SUBSCRIBER', 'Message content consumer'),
+       ('EB932DBB7005422FA6497190AF39E984', 'PRODUCER', 'Message content producer');
+
+INSERT INTO users (id, name, email, password, created, last_visit_id)
+VALUES ('0DD03A597DBC4D0081073271B3345434', 'Angela Merkel', 'angela@merkel.de', 'password', '1504249224',
+        '8A0BE660EBC64C47AF813F7123977993'),
+       ('1CD89E11602A4186AFBFE0149B59EB08', 'Emmanuel Macron', 'emmanuel@macron.fr', 'password', '1504249224',
+        'BE76648F49F24FBE9BC434BD8C23C70E'),
+       ('6E27EA06A7164C89AF88813749A8BD48', 'Donald Trump', 'donalt@trump.us', 'password', '1604129987',
+        'BE76648F49F24FBE9BC434BD8C23C70E'),
+       ('70A64B5443C34C18BBEC64590FF7E0CC', 'Justing Trudeau', 'justin@trudeau.ca', 'password', '1504249224',
+        '3C37571B0F494FED875845BFAE428B29'),
+       ('ABB04B9F5D1040DD9076EB27CA76891A', 'Vladimir Putin', 'vladimir@putin.tu', 'password', '1504249224',
+        '7AC613D5538745E58C9F9F49A0F271E1');
+
+INSERT INTO users_has_roles (users_id, roles_id)
+VALUES ('0DD03A597DBC4D0081073271B3345434', 'B479B3577E2547FA8DBABFDAEECC6C2C'),
+       ('1CD89E11602A4186AFBFE0149B59EB08', 'B479B3577E2547FA8DBABFDAEECC6C2C'),
+       ('1CD89E11602A4186AFBFE0149B59EB08', 'EB932DBB7005422FA6497190AF39E984'),
+       ('6E27EA06A7164C89AF88813749A8BD48', 'B479B3577E2547FA8DBABFDAEECC6C2C'),
+       ('6E27EA06A7164C89AF88813749A8BD48', 'EB932DBB7005422FA6497190AF39E984'),
+       ('70A64B5443C34C18BBEC64590FF7E0CC', 'B479B3577E2547FA8DBABFDAEECC6C2C'),
+       ('ABB04B9F5D1040DD9076EB27CA76891A', 'B479B3577E2547FA8DBABFDAEECC6C2C');
